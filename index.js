6981d614ad221a038c63d11a894dddd3033efe2b
lan-party <landon.burress@gmail.com>
2025-02-04 11:32:44 -0600

    fixed env variable typo

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ async function sendNotification(webpagePrevious, webpageCurrent) {
             
             var transporter = nodemailer.createTransport({
                 host: process.env.EMAIL_HOST,
-                port: process.env.EMAIL_POST,
+                port: process.env.EMAIL_PORT,
                 auth: {
                   user: process.env.EMAIL_USERNAME,
                   pass: process.env.EMAIL_PASSWORD
